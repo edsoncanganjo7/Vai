fc9a895da7f6ef7a5828055d9dff9d927e54035c
Edson Canganjo <edsoncanganjo@duck.com>
2025-04-14 18:08:32 +0100

    refactoring projects

---
+++ Vai.Parse/Parser.cs
@@ -0,0 +1,15 @@
+﻿namespace Vai.Parse;
+
+public class Parser : IParser
+{
+    public static int ParseToInt(ReadOnlySpan<char> s)
+    {
+        if (s.IsEmpty) return 0;
+
+        if (!int.TryParse(s.ToString(), out int result)) return 0;
+
+        return result;
+    }
+}
+
+public interface IParser { }
---
+++ Vai.Testes/ParserTests.cs
@@ -0,0 +1,19 @@
+﻿using Vai.Parse;
+
+namespace Vai.Testes;
+
+public class ParserTests
+{
+    [Fact]
+    public void Parser_ShouldBe_Ok()
+    {
+        // Arrange
+        string number = "1";
+
+        // Act
+        int result = Parser.ParseToInt(number.AsSpan());
+
+        // Assert
+        Assert.Equal(1, result);
+    }
+}
